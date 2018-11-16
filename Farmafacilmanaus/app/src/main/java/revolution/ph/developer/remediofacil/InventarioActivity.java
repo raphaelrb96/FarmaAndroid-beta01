986dd222a110fe3726a977c44037c6f557261271
Raphael Almeida <raphael.rb96@gmail.com>
2018-11-16 01:15:27 -0300

    Firebase auth, firestore, uiAuth, cloudMessaging

---
+++ Farmafacilmanaus/app/src/main/java/revolution/ph/developer/remediofacil/InventarioActivity.java
@@ -0,0 +1,13 @@
+package revolution.ph.developer.remediofacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario);
+    }
+}
